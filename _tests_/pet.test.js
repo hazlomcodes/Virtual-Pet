d7cf4e734c072297a5fd018101dad4680d48d756
hazlomcodes <harry.lomas1@outlook.com>
2022-11-08 16:29:35 +0000

    pass hunger test

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -1,11 +1,39 @@
 const Pet = require("../src/pet")
 
-describe ('constructor', () => {
-    it ('returns an object', () => {
-        expect (new Pet('Fido')).toBeInstanceOf(Object);
+describe('constructor', () => {
+    it('returns and object', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
 
-    it('returns an object with a name property' () => {
-        expect (new Pet("Gizmo").name).toBe("Gizmo");
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual ('Fido');
+
+    }); 
+
+    it('has an initail age of 0', () => {
+        const pet = new Pet('Fido'); 
+        expect(pet.age).toEqual(0);
     });
+
+    it('has an initial hunger of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+
+    describe('growUp',() => {
+        it('increments the age by 1', () => {
+            const pet= new Pet('Fido');
+            pet.growUp();
+            expect(pet.age).toEqual(1);
+        });
+    });
+
+    it('increase the hunger by 5', () => {
+        const pet= new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+
+
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,13 @@
 function Pet (name) {
 
+    this.name = name;
+    this.age = 0;
+    this.hunger= 0;
+}
+
+Pet.prototype.growUp = function() {
+    this.age +=1;
+    this.hunger += 5;
 };
 
 module.exports = Pet;
